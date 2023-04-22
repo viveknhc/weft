7cf6dea10d38d644a1c4af15b9e9b33cad227469
viveknhc <viveknhc@gmail.com>
2023-04-23 01:18:27 +0530

    22

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+document.writeln('<script src="assets/js/tweenMax.min.js" type="text/javascript"></script>');
+
+
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
       loop: true,
@@ -16,3 +19,62 @@ $(document).ready(function () {
       }
     });
   });
+
+
+  $(document).ready(function () {
+    $("#servie-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true, // enable navigation buttons
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+    });
+  });
+
+  $(document).ready(function () {
+    $("#news-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true, // enable navigation buttons
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 3
+        }
+      }
+    });
+  });
+
+
+  // ANIMATION
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry.target.getAttribute("data-animation"));
+
+      const animation = entry.target.getAttribute("data-animation");
+
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animated", `${animation}`);
+      } else {
+        entry.target.classList.remove("animated", `${animation}`);
+      }
+    });
+  });
+
+  const animatedEls = document.querySelectorAll("[data-animation]");
+  animatedEls.forEach((el) => observer.observe(el));
